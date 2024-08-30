39fdab6d3168599e192da7ca1da5b063b4cc0014
elyoh <elyoh@users.noreply.github.com>
2024-08-30 08:33:12 +0100

    Correct test.

--- src/libraries/System.Runtime/tests/System.Globalization.Calendars.Tests/System/Globalization/ChineseLunisolarCalendarTests.cs
+++ src/libraries/System.Runtime/tests/System.Globalization.Calendars.Tests/System/Globalization/ChineseLunisolarCalendarTests.cs
@@ -16,33 +16,36 @@ namespace System.Globalization.Tests
         [Fact]
         public void OffByOneDay()
         {          
-            // The lunar new moon separating lunar months 8/9 in 2057, 7/8 in 2089, and 6/7 in 2097 
-            // occurs close to midnight local time (UTC+8). The exact time cannot be determined accurately in advance.
-            // This may lead to off-by-one-day errors if the predictions made by the astronomical calculations 
-            // turn out to be wrong and the current table entries for 2057, 2089, and 2097,
-            // validated against Calendrical Calculations (Ultimate Edition), are changed.
-            // If these entries are adjusted in the future, this test will fail.
-            
-            // Lunar month 8 of 2057 has 30 days
-            Assert.AreEqual(Calendar.GetDaysInMonth(2057, 8), 30);
+            // The new moon separating lunar months 8/9 in 2057, 7/8 in 2089, 
+            // and 6/7 in 2097 occurs close to midnight local time (UTC+8). 
+            // The exact time cannot be determined accurately in advance.
+            // This may lead to off-by-one-day errors if the predictions made 
+            // by astronomical calculations turn out to be wrong. 
+            // The current table entries for 2057, 2089, and 2097 are validated 
+            // using 'Calendrical Calculations (Ultimate Edition)'. 
+            // This test guards against accidental regression, should future 
+            // recalibrations be required.
 
-            // Lunar month 9 of 2057 has 29 days and starts on 29 Sep 2057
-            Assert.AreEqual(Calendar.GetDaysInMonth(2057, 9), 29);
-            Assert.AreEqual(new DateOnly(2057, 9, 1, Calendar), new DateOnly(2057, 9, 28));
+            // Lunar month 8 of 2057 has 30 days
+            Assert.Equal(30, Calendar.GetDaysInMonth(2057, 8));
+
+            // Lunar month 9 of 2057 has 29 days and starts on 28 Sep 2057
+            Assert.Equal(29, Calendar.GetDaysInMonth(2057, 9));
+            Assert.Equal(new DateOnly(2057, 9, 28), new DateOnly(2057, 9, 1, Calendar));
 
             // Lunar month 7 of 2089 has 29 days
-            Assert.AreEqual(Calendar.GetDaysInMonth(2089, 7), 29); 
+            Assert.Equal(29, Calendar.GetDaysInMonth(2089, 7)); 
 
             // Lunar month 8 of 2089 has 30 days and starts on 04 Sep 2089
-            Assert.AreEqual(Calendar.GetDaysInMonth(2089, 8), 30); 
-            Assert.AreEqual(new DateOnly(2089, 8, 1, Calendar), new DateOnly(2089, 9, 4));
+            Assert.Equal(30, Calendar.GetDaysInMonth(2089, 8)); 
+            Assert.Equal(new DateOnly(2089, 9, 4), new DateOnly(2089, 8, 1, Calendar));
 
             // Lunar month 6 of 2097 has 29 days
-            Assert.AreEqual(Calendar.GetDaysInMonth(2097, 6), 29);
+            Assert.Equal(29, Calendar.GetDaysInMonth(2097, 6));
 
             // Lunar month 7 of 2097 has 30 days and starts on 07 Aug 2097
-            Assert.AreEqual(Calendar.GetDaysInMonth(2097, 7), 30); 
-            Assert.AreEqual(new DateOnly(2097, 7, 1, Calendar), new DateOnly(2097, 8, 7));
+            Assert.Equal(30, Calendar.GetDaysInMonth(2097, 7)); 
+            Assert.Equal(new DateOnly(2097, 8, 7), new DateOnly(2097, 7, 1, Calendar));
         }
     }
 }
